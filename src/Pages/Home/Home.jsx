1ee649e64f418e7efb772c64ae60b5a6c7f6cfb8
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2023-11-06 00:48:18 +0600

    added banner section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,9 @@
+import Banner from "./Banner";
 
 const Home = () => {
       return (
             <div>
-                  This is Home components
+                  <Banner/>
             </div>
       );
 };

90b43cee1aed5f0f72b6f347fe5c8540c416e87b
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2023-11-06 01:16:45 +0600

    added 404 page

--- src/Route/MainRoute.jsx
+++ src/Route/MainRoute.jsx
@@ -2,12 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../Root/Root";
 import Home from "../Pages/Home/Home";
 import Register from "../Pages/Register";
+import ErrorPage from "../Pages/ErrorPage";
 
 
 const router = createBrowserRouter([
       {
             path: '/',
             element: <Root></Root>,
+            errorElement: <ErrorPage></ErrorPage>,
             children: [
                   {
                         index: true,
